b664424a46e9b76e5dfc409e006221bcfdd2d693
Deepak <deepak.valluru@aa.com>
2020-01-08 01:53:23 -0600

    Adding custom object mapper for message converter

--- src/main/java/com/deepak/api/pokerservice/PokerServiceApplication.java
+++ src/main/java/com/deepak/api/pokerservice/PokerServiceApplication.java
@@ -1,8 +1,12 @@
 package com.deepak.api.pokerservice;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.CacheControl;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,6 +25,18 @@ public class PokerServiceApplication implements WebMvcConfigurer
       // Register resource handler for images
       registry.addResourceHandler("/api/pokergame/images/**").addResourceLocations("classpath:/images/")
               .setCacheControl( CacheControl.maxAge( 2, TimeUnit.HOURS).cachePublic());
+   }
+
+   @Bean
+   public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+      MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.configure( DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+      objectMapper.configure( DeserializationFeature.FAIL_ON_UNRESOLVED_OBJECT_IDS, false);
+      objectMapper.configure( DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+      objectMapper.configure( DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
+      jsonConverter.setObjectMapper(objectMapper);
+      return jsonConverter;
    }
 
 }
